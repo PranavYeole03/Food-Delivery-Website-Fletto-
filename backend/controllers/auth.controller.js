4445941212bfef0d65a184f4f753c6ccd532fe47
Pranav Yeole <yeolepranav13@gmail.com>
2026-04-06 16:48:25 +0530

    first commit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -32,8 +32,8 @@ export const signUp = async (req, res) => {
 
     const token = await genToken(user._id);
     res.cookie("token", token, {
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
     });
@@ -61,8 +61,8 @@ export const signIn = async (req, res) => {
     const token = genToken(user._id);
 
     res.cookie("token", token, {
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
     });
@@ -198,8 +198,8 @@ export const googleAuth = async (req, res) => {
     }
     const token = await genToken(user._id);
     res.cookie("token", token, {
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
     });
