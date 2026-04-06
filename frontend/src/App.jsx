4c3425271a617be798842e87cdbb37e6f85340eb
Pranav Yeole <yeolepranav13@gmail.com>
2026-04-06 16:49:47 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,161 +1,7 @@
-// import React from "react";
-// import { Navigate, Route, Routes } from "react-router-dom";
-// import SignUp from "./pages/SignUp";
-// import SignIn from "./pages/SignIn";
-// import ForgotPassword from "./pages/ForgotPassword";
-// import useGetCurrentUser from "./hooks/useGetCurrentUser";
-// import { useDispatch, useSelector } from "react-redux";
-// import Home from "./pages/Home";
-// import useGetCity from "./hooks/useGetCity";
-// import useGetMyShop from "./hooks/useGetMyShop";
-// import CreateEditShop from "./pages/CreateEditShop";
-// import AddItem from "./pages/AddItem";
-// import EditItem from "./pages/EditItem";
-// import useGetShopByCity from "./hooks/useGetShopByCity";
-// import useGetItemByCity from "./hooks/useGetItemByCity";
-// import CartPages from "./pages/CartPages";
-// import CheckOut from "./pages/CheckOut";
-// import OrderPlaced from "./pages/OrderPlaced";
-// import MyOrders from "./pages/MyOrders";
-// import useGetMyOrders from "./hooks/useGetMyOrders";
-// import useUpdateLocation from "./hooks/useUpdateLocation";
-// import TrackOrderPage from "./pages/TrackOrderPage";
-// import ShopItems from "./pages/ShopItems";
-// import { useEffect } from "react";
-// import { io } from "socket.io-client";
-// import { setSocket } from "./redux/userSlice";
-
-// export const serverUrl = "http://localhost:8000";
-
-// const App = () => {
-//   const { userData } = useSelector((state) => state.user);
-//   const dispatch = useDispatch();
-//   useGetCurrentUser();
-//   useGetCity();
-//   useGetMyShop();
-//   useGetShopByCity();
-//   useGetItemByCity();
-//   useGetMyOrders();
-//   useUpdateLocation();
-
-//   useEffect(() => {
-//     const socketInstance = io(serverUrl, { withCredentials: true });
-//     dispatch(setSocket(socketInstance));
-//     socketInstance.on("connect", () => {
-//       if (userData) {
-//         socketInstance.emit("identity", { userId: userData._id });
-//       }
-//     });
-//     return () => {
-//       socketInstance.disconnect()
-//     }
-
-//   }, [userData, dispatch]);
-//   useEffect(() => {
-//     if (userData) {
-//       // Prevent duplicate loading
-//       if (!document.getElementById("fletto-chatbot")) {
-//         const script = document.createElement("script");
-//         script.id = "fletto-chatbot";
-//         script.src = "https://ai-chat-bot-five-lake.vercel.app/chatBot.js";
-//         script.setAttribute("data-owner-id", "usr_110779774695178759");
-//         script.async = true;
-
-//         document.body.appendChild(script);
-//       }
-//     } else {
-//       // Remove chatbot on logout
-//       const existing = document.getElementById("fletto-chatbot");
-//       if (existing) existing.remove();
-//     }
-//   }, [userData]);
-
-
-
-//   useEffect(() => {
-//     if (userData) {
-//       if (!document.getElementById("fletto-chatbot")) {
-//         const script = document.createElement("script");
-//         script.id = "fletto-chatbot";
-//         script.src = "https://ai-chat-bot-five-lake.vercel.app/chatBot.js";
-//         script.setAttribute("data-owner-id", "usr_110779774695178759");
-//         script.async = true;
-
-//         document.body.appendChild(script);
-//       }
-//     } else {
-//       // Remove chatbot on logout
-//       const existing = document.getElementById("fletto-chatbot");
-//       if (existing) existing.remove();
-//     }
-//   }, [userData]);
-
-
-//   return (
-//     <Routes>
-//       <Route
-//         path="/signup"
-//         element={!userData ? <SignUp /> : <Navigate to={"/"} />}
-//       />
-//       <Route
-//         path="/signin"
-//         element={!userData ? <SignIn /> : <Navigate to={"/"} />}
-//       />
-//       <Route
-//         path="/forgot-password"
-//         element={!userData ? <ForgotPassword /> : <Navigate to={"/"} />}
-//       />
-//       <Route
-//         path="/"
-//         element={userData ? <Home /> : <Navigate to={"/signin"} />}
-//       />
-//       <Route
-//         path="/create-edit-Shop"
-//         element={userData ? <CreateEditShop /> : <Navigate to={"/signin"} />}
-//       />
-//       <Route
-//         path="/add-items"
-//         element={userData ? <AddItem /> : <Navigate to={"/signin"} />}
-//       />
-//       <Route
-//         path="/edit-items/:itemId"
-//         element={userData ? <EditItem /> : <Navigate to={"/signin"} />}
-//       />
-//       <Route
-//         path="/cart"
-//         element={userData ? <CartPages /> : <Navigate to={"/signin"} />}
-//       />
-//       <Route
-//         path="/checkOut"
-//         element={userData ? <CheckOut /> : <Navigate to={"/signin"} />}
-//       />
-//       <Route
-//         path="/order-place"
-//         element={userData ? <OrderPlaced /> : <Navigate to={"/signin"} />}
-//       />
-//       <Route
-//         path="/my-order"
-//         element={userData ? <MyOrders /> : <Navigate to={"/signin"} />}
-//       />
-//       <Route
-//         path="/track-order/:orderId"
-//         element={userData ? <TrackOrderPage /> : <Navigate to={"/signin"} />}
-//       />
-//       <Route
-//         path="/shop-items/:shopId"
-//         element={userData ? <ShopItems /> : <Navigate to={"/signin"} />}
-//       />
-//     </Routes>
-//   );
-// };
-
-// export default App;
 import React, { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { socket } from "./socket";
-
-// Pages
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -171,8 +17,6 @@ import TrackOrderPage from "./pages/TrackOrderPage";
 import ShopItems from "./pages/ShopItems";
 import Analytics from "./pages/Analytics";
 import AppInfo from "./pages/AppInfo"
-
-// Hooks
 import useGetCurrentUser from "./hooks/useGetCurrentUser";
 import useGetCity from "./hooks/useGetCity";
 import useGetMyShop from "./hooks/useGetMyShop";
@@ -180,8 +24,6 @@ import useGetShopByCity from "./hooks/useGetShopByCity";
 import useGetItemByCity from "./hooks/useGetItemByCity";
 import useGetMyOrders from "./hooks/useGetMyOrders";
 import useUpdateLocation from "./hooks/useUpdateLocation";
-
-// Layout
 import OwnerLayout from "./components/OwnerLayout";
 import OwnerDashboard from "./components/OwnerDashboard";
 import { serverUrl } from "./config";
